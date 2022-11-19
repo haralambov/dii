349141ba49531d93e28fa9b275a86825c1f9d407
Zlatomir Haralambov <z.haralambov@gmail.com>
2022-11-19 14:48:29 +0200

    Adds solaar to the programs list

--- dii.sh
+++ dii.sh
@@ -187,7 +187,7 @@ function install_programs() {
         "blueman" "i3blocks" "lxappearance" 
         "network-manager" "network-manager-gnome" # for applet
         "xautolock" "diodon" "compton"
-        "xdotool"
+        "xdotool" "solaar"
     )
 
     for PROGRAM in "${PROGRAMS[@]}"; do
